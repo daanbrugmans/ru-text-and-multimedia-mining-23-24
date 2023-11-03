e50c8a9496ca32902b57af29561148aa19fea7ef
daanbrugmans <daanbrugmans@gmail.com>
2023-11-03 19:04:03 +0100

    Reran featurizer for all features

--- Assignments/Assignment 4/code/authorship_attribution.py
+++ Assignments/Assignment 4/code/authorship_attribution.py
@@ -7,16 +7,16 @@ from classifier import Classifier
 
 
 RERUN_FEATURES = False
-# os.chdir('[/home/janneke/Documents/Master/Text and Multimedia Mining/Assignment 4/code]')
 
 
 def main():
-    os.chdir(os.path.join(os.getcwd(), "Assignments", "Assignment 4"))
+    data_dir = (os.path.join(os.getcwd(), "Assignments", "Assignment 4", "data"))
+    
     featurizer = Featurizer()
 
-    full_train_set = get_features(featurizer, RERUN_FEATURES, feature_filename="../data/train_features.csv", filename="../data/pan2324_train_data.csv")
-    full_dev_set = get_features(featurizer, RERUN_FEATURES, feature_filename="../data/dev_features.csv", filename="../data/pan2324_dev_data.csv")
-    # full_test_set = get_features(featurizer, RERUN_FEATURES, feature_filename="../data/test_features.csv", filename="../data/pan2324_test_data.csv")
+    full_train_set = get_features(featurizer, RERUN_FEATURES, feature_filename=os.path.join(data_dir, "train_features.csv"), filename=os.path.join(data_dir, "pan2324_train_data.csv"))
+    full_dev_set = get_features(featurizer, RERUN_FEATURES, feature_filename=os.path.join(data_dir, "dev_features.csv"), filename=os.path.join(data_dir, "pan2324_dev_data.csv"))
+    # full_test_set = get_features(featurizer, RERUN_FEATURES, feature_filename=os.path.join(data_dir, "test_features.csv"), filename=os.path.join(data_dir, "pan2324_test_data.csv"))
 
     print("Running classifier...")
     classifier = Classifier()
@@ -30,18 +30,21 @@ def main():
     classifier.evaluate(dev_x, dev_y)
 
 
-def get_features(featurizer: Featurizer, rerun_features, feature_filename, filename):
+def get_features(featurizer: Featurizer, rerun_features, feature_filename, filename):   
     data_set = pd.read_csv(filename, index_col=0)
+    data_set.index.names = ["index"]
+    
     if rerun_features or not os.path.exists(feature_filename):
         print("Calculating features...")
         features = featurizer.featurize(data_set)
         features.to_csv(feature_filename)
         summary = features.describe(include='all')
-        summary.to_csv(f"{'.'.join(feature_filename.split('.')[:-1])}.csv")
+        summary.to_csv(f"{feature_filename.split('.')[0].split('_')[0]}_summary.csv")
     else:
         print("Loading features...")
-        features = pd.read_csv(feature_filename)
-    return data_set.merge(features, left_index=True, right_index=True)
+        features = pd.read_csv(feature_filename, index_col="index")
+
+    return pd.merge(data_set, features, on="index", how="inner")
     
 
 if __name__ == "__main__":
